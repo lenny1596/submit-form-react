0908f7d3b07a0bf542ebc87684b925867d2f17d8
Lenin <leninbose1596@gmail.com>
2024-04-28 21:20:41 +0530

    added state for form data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div>Hello Lenin!</div>;
-}
+const App = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    contact: "",
+    gender: "",
+    resume: "",
+    url: "",
+    selectedOption: "",
+    about: "",
+    subject: {
+      english: true,
+      math: false,
+      physics: false,
+      chemistry: true,
+    },
+  });
+
+  return (
+    <div className="App">
+    </div>
+  );
+};
 
 export default App;
